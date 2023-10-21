b1502019c0f3cb17dec705db5db2ab2be79313f1
Nitin31-SE <niky31032003@gmail.com>
2023-10-21 20:55:07 +0530

    Create App.java

---
+++ util/App.java
@@ -0,0 +1,15 @@
+package util;
+
+public class App {
+    package com.houarizegai.calculator;
+
+import com.houarizegai.calculator.ui.CalculatorUI;
+
+public class App {
+
+    public static void main(String[] args) {
+        new CalculatorUI();
+    }
+}
+
+}
